2a565b62ef8c32cc6b2de047d2e709ba4701b3c9
Joe <josephhaenel@gmail.com>
2023-08-29 22:50:54 -0500

    Using Classes

---
+++ RScraper_v0.1.2.py
@@ -0,0 +1,122 @@
+'''
+    File:   RScraper_v0.1.2.py
+    By:     Joseph Haenel
+    Date:   08-29-2023
+    Dependencies: Selenium, BeautifulSoup4, ChromeDriver.exe
+    Using Python 3.11.0
+'''
+
+import time
+import os
+import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+
+
+class RedditScraper:
+    ''' Reddit Scraper class to scrape information from Reddit subreddits '''
+
+    def __init__(self, subreddit, scrolls=3, max_posts=10, output_dir='IO'):
+        self.MAX_POSTS = max_posts
+        self.BASE_URL = 'https://www.reddit.com/r/'
+        self.subreddit = subreddit
+        self.scrolls = scrolls
+        self.output_file = os.path.join(
+            os.path.dirname(__file__), output_dir, 'output.json')
+
+    def getSoupObj(self):
+        '''Returns a BeautifulSoup object for a given subreddit'''
+        driver_path = os.path.join(os.path.dirname(
+            __file__), 'resources', 'chromedriver.exe')
+        driver = webdriver.Chrome(executable_path=driver_path)
+        driver.get(self.BASE_URL + self.subreddit)
+        time.sleep(2)
+        for _ in range(self.scrolls):
+            driver.implicitly_wait(10)
+            driver.find_element_by_tag_name("body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(3)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        driver.quit()
+        return soup
+
+    def getTotalMembers(self, subredditSoupObj):
+        '''Returns the total number of members in a given subreddit'''
+        membersIdentifier = subredditSoupObj.find("faceplate-number")
+        if not membersIdentifier:
+            raise Exception(
+                f"Members identifier of {subredditSoupObj} not found")
+        return int(membersIdentifier['number'])
+
+    def getTitle(self, subredditSoupObj):
+        '''Returns the title of a subreddit post'''
+        postTitle = subredditSoupObj.find("div", {"slot": "title"})
+        if not postTitle:
+            raise Exception(f"Post title for {subredditSoupObj} not found")
+        return postTitle.text.strip()
+
+    def getAuthor(self, subredditSoupObj):
+        '''Returns the author of a subreddit post'''
+        author = subredditSoupObj.find("span", class_="whitespace-nowrap")
+        if not author:
+            raise Exception(f"Author for {subredditSoupObj} not found")
+        return author.text.strip()
+
+    def getTimeStamp(self, subredditSoupObj):
+        '''Returns the time stamp for a subreddit post'''
+        postTimeStamp = subredditSoupObj.find("faceplate-timeago")
+        if not postTimeStamp:
+            raise Exception(
+                f"Post time stamp for {subredditSoupObj} not found")
+        timeTag = postTimeStamp.find('time')
+        if not timeTag:
+            raise Exception(f"Post time tag for {postTimeStamp} not found")
+        return timeTag['datetime']
+
+    def getThumbsUp(self, subredditSoupObj):
+        '''Returns the number of "likes" for a subreddit post'''
+        thumbsUp = subredditSoupObj.get("score")
+        if not thumbsUp:
+            return 0
+        return thumbsUp
+
+    def scrape(self):
+        '''Scrape data from a subreddit and write it to output.json'''
+        subredditSoupObj = self.getSoupObj()
+        if not subredditSoupObj:
+            raise Exception(
+                f"Could not get subreddit object for {self.subreddit}")
+
+        results = []
+
+        totalMembers = self.getTotalMembers(subredditSoupObj)
+        totalMembersString = f"Members of r/{self.subreddit}: {totalMembers}\n"
+        print(totalMembersString)
+
+        results.append({"Subreddit": self.subreddit, "Members": totalMembers})
+
+        for index, post in enumerate(subredditSoupObj.find_all("shreddit-post")):
+            postData = {
+                "Title": self.getTitle(post),
+                "Author": self.getAuthor(post),
+                "TimeStamp": self.getTimeStamp(post),
+                "ThumbsUp": self.getThumbsUp(post)
+            }
+            results.append(postData)
+
+            if index > self.MAX_POSTS:
+                break
+
+        with open(self.output_file, 'w') as file:
+            json.dump(results, file, indent=4)
+
+
+if __name__ == '__main__':
+    with open(os.path.join(os.path.dirname(__file__), 'IO', 'subreddits.txt'), 'r') as f:
+        subreddits = [line.strip() for line in f]
+
+    for subreddit in subreddits:
+        print(f"Scraping {subreddit}...")
+        scraper = RedditScraper(subreddit)
+        scraper.scrape()
+        print(f"Finished scraping {subreddit}\n{'='*60}\n")
